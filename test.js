b2671a05f0fbf522baa08aacec9df09d9b6e641e
Patrik Vilja <patrik.vilja@gmail.com>
2015-01-01 23:04:06 +0200

    add a new base impl

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
+var stream = EventStream.fromEvent('click', document.querySelector('input'));
 
-var stream = EventStream(function (event) {
-  event(1);
-});
-
-stream.map(function (value) {
-  return value + 10;
-}).forEach(function (value) {
-  console.log(value);
-});
-
-
-var clickStream = EventStream.asEventStream('click', document.querySelector('input'));
-
-clickStream.map(function (e) {
-  return 1;
-}).map(function (value) {
-  return value + 2;
-}).forEach(function (value) {
-  console.log(value);
+stream.forEach(function (e) {
+  console.log(e);
 });
